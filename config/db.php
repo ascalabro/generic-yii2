fea8b73249f7f6401a0855e362b4fd4aef60b2b8
Angelo Scalabroni <armotek5421@msn.com>
2016-01-29 10:35:31 -0500

    Initial check-in

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.56.101;dbname=sandbox',
+    'username' => 'admin',
+    'password' => 'devadmin15',
+    'charset' => 'utf8',
+];
